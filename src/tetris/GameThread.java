2554198fab844c6a08608d637082389cb78ded35
User <natyamm10@gmail.com>
2021-12-11 22:21:32 +0800

    Update

---
+++ src/tetris/GameThread.java
@@ -0,0 +1,33 @@
+package tetris;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class GameThread extends Thread
+{
+    private GameArea ga;
+    
+    public GameThread(GameArea ga)
+    {
+     this.ga = ga;   
+    }
+    
+    @Override
+    public void run()
+    {
+        while(true)
+        {
+            ga.spawnBlock();
+            
+            while( ga.moveBlockDown())
+            {
+                 try {
+                    Thread.sleep(1000);
+                } 
+                catch (InterruptedException ex) {
+                    Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE, null, ex);
+                } 
+            }
+        } 
+    }
+}
